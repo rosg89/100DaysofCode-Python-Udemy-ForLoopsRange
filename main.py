b1ca72f94e3e83751c595d8913001ce285e3597b
rosg89 <rsuarezgalan@hotmail.com>
2022-05-19 16:37:00 -0300

    Se sube el proyecto

---
+++ main.py
@@ -0,0 +1,18 @@
+#For Loop with Range
+#Imprime del 1 al 9
+#Incrementa por 1
+for number in range(1,10):
+  print(number)
+
+#For Loop with Range
+#Imprime del 1 al 9
+#Incrementa por 3
+for number in range(1,10,3):
+  print(number)
+
+#Sumar todos los numeros de 1 a 100
+total = 0
+for number in range (1,101):
+  total += number
+
+print(total)
